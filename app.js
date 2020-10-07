e44a61169ff321c7bb44031e593d335b9b312bdc
Necritto <onetopyr@mail.ru>
2020-10-07 13:16:22 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} port...`);
+});
