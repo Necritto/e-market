221bdc63b254ccdc8f72db69ed17898b78f53c74
Necritto <onetopyr@mail.ru>
2020-10-08 20:58:07 +0300

    Feature: post request processing [ref:no-ref]

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const Course = require("../models/course");
+
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -6,6 +8,15 @@ router.get("/", (req, res) => {
     title: "Add course",
     isAdd: true,
   });
+});
+
+router.post("/", async (req, res) => {
+  const { title, price, img } = req.body;
+  const course = new Course(title, price, img);
+
+  await course.save();
+
+  res.redirect("/courses");
 });
 
 module.exports = router;
