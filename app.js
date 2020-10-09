f8a8c505829244819f0a4055ea236e3f0d5096b3
Necritto <onetopyr@mail.ru>
2020-10-09 18:47:22 +0300

    Feature: cart routes [ref:no-ref]

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const homeRouter = require("./routes/home");
 const coursesRouter = require("./routes/courses");
 const addRouter = require("./routes/add");
+const cartRouter = require("./routes/cart");
 
 const app = express();
 
@@ -16,11 +18,12 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", homeRouter);
 app.use("/courses", coursesRouter);
 app.use("/add", addRouter);
+app.use("/cart", cartRouter);
 
 const PORT = process.env.PORT || 3000;
 
