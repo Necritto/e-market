a8ab60ad2cc70085e2802440e1d648d0dde6ed6c
Necritto <onetopyr@mail.ru>
2020-10-17 18:43:36 +0300

    Feature: courses validation [ref:no-ref]

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const Course = require("../models/course");
 const auth = require("../middleware/auth");
+const { validationResult } = require("express-validator");
+const { courseValidators } = require("../utils/validators");
 
 const router = Router();
 
@@ -60,9 +62,15 @@ router.get("/:id/edit", auth, async (req, res) => {
   }
 });
 
-router.post("/edit", auth, async (req, res) => {
+router.post("/edit", auth, courseValidators, async (req, res) => {
   try {
+    const errors = validationResult(req);
     const { id } = req.body;
+
+    if (!errors.isEmpty()) {
+      return res.status(422).redirect(`/courses/${id}/edit?allow=true`);
+    }
+
     delete req.body.id;
     const course = await Course.findById(id);
 
